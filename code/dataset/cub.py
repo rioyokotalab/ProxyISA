c1c6e28db243bf4508c27737ba00f5e5ab3e0278
Lyle-Lee <lyleleeao@gmail.com>
2021-06-08 01:00:32 +0900

    update cub preprocess

--- code/dataset/cub.py
+++ code/dataset/cub.py
@@ -13,14 +13,15 @@ class CUBirds(BaseDataset):
         
         BaseDataset.__init__(self, self.root, self.mode, self.transform)
         index = 0
-        limit = 60
-        capacity = [limit for x in range(10)]
-        for c in range(1, 10):
-            if limit * self.scale > 5:
-                limit *= self.scale
-            else:
-                limit = 5
-            capacity += [int(limit) for x in range(10)]
+        if self.scale < 1.0:
+            limit = 60
+            capacity = [limit for x in range(10)]
+            for c in range(1, 10):
+                if limit * self.scale > 5:
+                    limit *= self.scale
+                else:
+                    limit = 5
+                capacity += [int(limit) for x in range(10)]
         #container = [0 for x in range(100)]
         for i in torchvision.datasets.ImageFolder(root = 
                 os.path.join(self.root, 'images')).imgs:
@@ -29,7 +30,7 @@ class CUBirds(BaseDataset):
             # fn needed for removing non-images starting with `._`
             fn = os.path.split(i[0])[1]
             if y in self.classes and fn[:2] != '._':
-                if self.mode == 'train':
+                if self.scale < 1.0 and self.mode == 'train':
                     if capacity[y] > 0:
                         self.ys += [y]
                         self.I += [index]
